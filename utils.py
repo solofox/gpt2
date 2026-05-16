a34747070bbecc54811e8173de9417f3ce09917c
solofox <wensg100@sina.com>
2026-05-17 06:20:20 +0800

    fixed safetensors loader

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from os import PathLike
 import json
 import struct
@@ -22,7 +22,7 @@ SAFETENSORS_DTYPE_MAP = {
     "U8": torch.uint8,
     "BOOL": torch.bool,
 }
-def _load_tensor(fp, tensor_name: str, dtype_name: str, data_offsets: Tuple[int, int], shape: List[int]) -> torch.Tensor:
+def _load_tensor(fp, tensor_name: str, dtype_name: str, data_offsets: Tuple[int, int], shape: List[int], header_size: int) -> torch.Tensor:
     if dtype_name not in SAFETENSORS_DTYPE_MAP:
         raise Exception(f"Tensor {tensor_name}: unknown data type {dtype_name}")
     dtype = SAFETENSORS_DTYPE_MAP[dtype_name]
@@ -34,7 +34,7 @@ def _load_tensor(fp, tensor_name: str, dtype_name: str, data_offsets: Tuple[int,
         total_elements *= i
     if total_elements * dtype.itemsize != buffer_len:
         raise Exception(f"Tensor {tensor_name}: data offsets can't match shape.")
-    fp.seek(data_offsets[0], os.SEEK_SET)
+    fp.seek(header_size + data_offsets[0], os.SEEK_SET)
     buffer = fp.read(buffer_len)
     if len(buffer) != buffer_len:
         raise Exception(f"Tensor {tensor_name}: data is too short in file, is it truncated?")
@@ -64,7 +64,29 @@ def load_safetensors(path: PathLike):
                 or 'data_offsets' not in tensor_info \
                 or 'shape' not in tensor_info:
                 raise Exception(f"Invalid tensor {key}, not dtype/data_offsets/shape all present: {tensor_info}")
-            tensor = _load_tensor(f, key, tensor_info['dtype'], tensor_info['data_offsets'], tensor_info['shape'])
+            tensor = _load_tensor(f, key, tensor_info['dtype'], tensor_info['data_offsets'], tensor_info['shape'], 8 + header_len)
             tensor_info['tensor'] = tensor
         return header
     
+def tensors_compare():
+    import safetensors
+    import pathlib
+    #import pdb; pdb.set_trace()
+    path = pathlib.Path('./models/chatgpt2-small/model.safetensors')
+    mydata = load_safetensors(path)
+    passed = True
+    with safetensors.safe_open(path, framework="pt") as f:
+        for k in f.keys():
+            my = mydata[k]['tensor']
+            other = f.get_tensor(k)
+            diff = my - other
+            sum_diff = torch.sum(diff ** 2).item()
+            avergage_diff =  sum_diff / diff.numel()
+            this_passed = (sum_diff < 1e-3) and (avergage_diff < 1e-9)
+            print(f"comparing {k}: total diff={sum_diff}, average diff={avergage_diff}, passed={this_passed}")
+            passed = passed and this_passed
+    print("PASSED" if passed else "FAILED")
+    return 0 if passed else 1
+
+if __name__ == "__main__":
+    sys.exit(tensors_compare())
